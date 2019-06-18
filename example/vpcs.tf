9e5a5944c95863bad727f1150d7e40baefc4ba70
chavo1 <chavdar.rakov@gmail.com>
2019-06-18 13:04:06 +0300

    refactor

--- example/vpcs.tf
+++ example/vpcs.tf
@@ -13,18 +13,18 @@ module "vpc_virginia" {
   mapPublicIP          = "${var.mapPublicIP}"
 }
 
-# module "vpc_ohio" {
-#   source = "github.com/chavo1/aws-vpc-terraform"
+module "vpc_ohio" {
+  source = "github.com/chavo1/aws-vpc-terraform"
 
-#   region               = "${var.region[1]}"
-#   vpc_name             = "${var.vpc_name[1]}"
-#   subnetCIDRblock      = "${var.subnetCIDRblock[1]}"
-#   ingressCIDRblock     = "${var.ingressCIDRblock}"
-#   instanceTenancy      = "${var.instanceTenancy}"
-#   dnsSupport           = "${var.dnsSupport}"
-#   vpcCIDRblock         = "${var.vpcCIDRblock[1]}"
-#   destinationCIDRblock = "${var.destinationCIDRblock}"
-#   availabilityZone     = "${var.availabilityZone[1]}"
-#   mapPublicIP          = "${var.mapPublicIP}"
+  region               = "${var.region[1]}"
+  vpc_name             = "${var.vpc_name[1]}"
+  subnetCIDRblock      = "${var.subnetCIDRblock[1]}"
+  ingressCIDRblock     = "${var.ingressCIDRblock}"
+  instanceTenancy      = "${var.instanceTenancy}"
+  dnsSupport           = "${var.dnsSupport}"
+  vpcCIDRblock         = "${var.vpcCIDRblock[1]}"
+  destinationCIDRblock = "${var.destinationCIDRblock}"
+  availabilityZone     = "${var.availabilityZone[1]}"
+  mapPublicIP          = "${var.mapPublicIP}"
 
-# }
+}
